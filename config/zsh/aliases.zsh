0ee6395bf1bdee2d029a7f6c1be52dc44d968e03
Philip Capel <insert.available.name@gmail.com>
2022-10-29 09:39:18 -0500

    Add zsh aliases

---
+++ config/zsh/aliases.zsh
@@ -0,0 +1,43 @@
+# Git aliases that I'm used to
+alias gap='git add -p'
+alias gb='git branch'
+alias gc='git commit -v'
+alias gca='git commit -a -v'
+alias gcl='git clean -f -d'
+alias gco='git checkout'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gdh='git diff HEAD'
+alias gl='git pull'
+alias glg='git log --graph --oneline --decorate --color --all'
+alias glod='git log --oneline --decorate'
+alias glp='git log -p'
+alias gnap='git add -N --ignore-removal . && gap && gref'
+alias gp='git push'
+alias gplease='git push --force-with-lease'
+alias gpr='git pull --rebase'
+alias gr='git rebase'
+alias gra='git rebase --abort'
+alias grc='git rebase --continue'
+alias grim='git rebase -i main'
+alias gst='git status'
+
+# Fun stuff for mix
+alias mget='mix deps.get'
+alias phs='mix phx.server'
+
+# Aliasing kubectl this way has the added benefit of causing the docker prompt to display the configured context prior to opening k9s
+alias k="kubectl"
+
+# Exa - https://the.exa.website/
+alias l="exa --group --header --classify --icons --group-directories-first"
+alias ls="exa --group --all --header --classify --icons --group-directories-first"
+alias ll="exa --long --header --git --all --group --icons --group-directories-first"
+
+# Common operations when not inside of a poetry shell
+alias pr="poetry run"
+alias pri="poetry run ipython"
+alias ppy="poetry  run python"
+
+# Tmux stuff
+alias tatt="tmux attach"
